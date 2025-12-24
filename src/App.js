1cf63420e5af8930fde70623f1626a69d00b1448
viziunetech <viziunetech@gmail.com>
2025-12-24 12:00:59 +0530

    removed empty

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import * as React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
 import './App.css';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
 import Convert from './pages/Convert';
 import View from './pages/View';
